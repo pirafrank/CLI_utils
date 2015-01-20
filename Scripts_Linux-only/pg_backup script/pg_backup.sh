b585868af28b65d3ef7bf398372a345fc67a386e
Francesco Pira <francescopira.me@gmail.com>
2015-01-20 11:25:05 +0100

    now creates needed parent folders for BACKUP_PATH

--- Scripts_Linux-only/pg_backup script/pg_backup.sh
+++ Scripts_Linux-only/pg_backup script/pg_backup.sh
@@ -32,7 +32,7 @@ DBNAME="unstable"
 
 # checking if folder exists, else create it
 if [[ ! -d $BACKUP_PATH ]]; then
-     mkdir $BACKUP_PATH
+     mkdir -p $BACKUP_PATH
 fi
 
 cd $BACKUP_PATH
@@ -40,10 +40,15 @@ mkdir $DATE
 cd $DATE
 
 # backing up db
-pg_dump --host=localhost --username=$DBUSER -w --format=t $DBNAME > "$DBNAME""_dump_$DATE.tar"
+pg_dump --host=localhost --username=$DBUSER -w --format=t $DBNAME > "${DBNAME}_dump_${DATE}.tar"
 
 # deleting unwanted backups
 rotate-backups --hourly=0 --daily=5 --weekly=2 --monthly=2 --yearly=2 $BACKUP_PATH
 
 # mirroring backup to 'yesterday' database
-pg_restore --host=localhost --username=$DBUSER -w --format=t -c --dbname=yesterday "$DBNAME""_dump_$DATE.tar"
+pg_restore --host=localhost --username=$DBUSER -w --format=t -c --dbname=yesterday "${DBNAME}_dump_${DATE}.tar"
+
+
+
+
+
