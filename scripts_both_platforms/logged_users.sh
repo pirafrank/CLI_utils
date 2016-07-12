938ef1746ba5148a0a55dc4691d1098b0a1cde05
Francesco Pira <dev@fpira.com>
2016-07-12 11:02:03 +0200

    clean up

--- scripts_both_platforms/logged_users.sh
+++ scripts_both_platforms/logged_users.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+#
+# logged_users.sh
+# Lists who and how many times is logged
+#
+# Francesco Pira <dev[at]fpira[dot]com>
+# Released under the terms of GNU GPLv3
+#
+
 if [[ $# > 1 ]]; then
     echo "Error: wrong number of arguments"
     exit -1
 else
-    if [[ $1 == "" ]]; then      
+    if [[ $1 == "" ]]; then
         while read -r line
         do
             USER=$(echo $line | awk '{print $2}')
@@ -13,9 +21,6 @@ else
         done < <(w | sed '1,2d' | cut -f1 -d' ' | sort | uniq -c)
     else
         FIRST_LETTER=$1
-        #echo "$FIRST_LETTER"
-        #INFO=$(w | grep "^$FIRST_LETTER")
-        #echo "$INFO"
         while read -r line
         do
             USER=$(echo $line | awk '{print $2}')
